9c04194e35982677756b40b936806c354ebdda00
fanghao.wang <fanghao.wang@happy-seed.com>
2020-07-31 19:00:54 +0800

    f

---
+++ src/Common.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class Common
+{
+    // 获取指定日期所在月的开始日期与结束日期
+    public function get_month_range($timestamp)
+    {
+        $ret = array();
+        $mdays = date('t', $timestamp);
+        $ret['sdate'] = strtotime(date('Y-m-1 00:00:00', $timestamp));
+        $ret['edate'] = strtotime(date('Y-m-' . $mdays . ' 23:59:59', $timestamp));
+        return $ret;
+    }
+
+}
